6571a2bee1e83f50244fee37ffbb1fc8e592e982
IhorUB <maryniuk94@ukr.net>
2020-12-03 00:59:16 +0200

    vuex modules for auth

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -27,7 +27,8 @@
                 type="password"
                 placeholder="Password"/>
             </fieldset>
-            <button class="btn btn-lg btn-primary pull-xs-right">
+            <button class="btn btn-lg btn-primary pull-xs-right"
+                    :disabled="isSubmitting">
               Sign Up
             </button>
           </form>
@@ -40,10 +41,16 @@
 <script>
 export default {
   name: "McvRegister",
+  computed: {
+    isSubmitting() {
+      return this.$store.state.auth.isSubmitting
+    }
+  },
   methods: {
     onSubmit() {
-      console.log("|")
-    }
+      console.log("submit");
+      this.$store.commit('registerStart');
+    },
   }
 }
 </script>
