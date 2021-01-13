dd535db2792296515fd63dc2b6597bf5dae6d0d7
IhorUB <maryniuk94@ukr.net>
2021-01-13 13:19:55 +0200

    add popularTags component

--- src/views/GlobalFeed.vue
+++ src/views/GlobalFeed.vue
@@ -6,15 +6,17 @@
         <div class="col-md-9">
           <mcv-feed :api-url="apiUrl"/>
         </div>
-        <div class="col-md-3">Popular Tags</div>
+        <div class="col-md-3">
+            <mcv-popular-tags/>
+        </div>
       </div>
     </div>
   </div>
-
 </template>
 
 <script>
 import McvFeed from '@/components/Feed'
+import McvPopularTags from '@/components/PopularTags'
 
 export default {
   name: "McvGlobalFeed",
@@ -24,7 +26,7 @@ export default {
     }
   },
   components: {
-    McvFeed
+    McvFeed, McvPopularTags
   }
 }
 </script>
