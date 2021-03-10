f9bbec184c5bdffbdbc8f90b6f071a211ae45fca
IhorYB <ihor.maryniukub@gmail.com>
2021-03-10 11:54:24 +0200

    create article form

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Login from '@/views/Login'
 import YourFeed from "@/views/YourFeed"
 import TagFeed from "@/components/TagFeed"
 import Article from "@/views/Article"
+import CreateArticle from "@/views/CreateArticle"
 
 Vue.use(VueRouter)
 
@@ -14,26 +15,26 @@ const routes = [{
     path: '/register', name: 'register', component: Register
 },
     {
-    path: '/login', name: 'login', component: Login
-}, {
-    path: '/', name: 'globalFeed', component: GlobalFeed
-}, {
-    path: '/feed', name: 'yourFeed', component: YourFeed
-}, {
-    path: '/tags/:slug', name: 'tag', component: TagFeed
-}, {
-    path: '/articles/new', name: 'createArticle', component: GlobalFeed
-}, {
-    path: '/articles/:slug', name: 'article', component: Article
-}, {
-    path: '/article/:slug/edit', name: 'editArticle', component: GlobalFeed
-}, {
-    path: '/settings', name: 'settings', component: GlobalFeed
-}, {
-    path: '/profiles/:slug', name: 'userProfile', component: GlobalFeed
-}, {
-    path: '/profiles/:slug/favorites', name: 'userProfileFavorites', component: GlobalFeed
-},]
+        path: '/login', name: 'login', component: Login
+    }, {
+        path: '/', name: 'globalFeed', component: GlobalFeed
+    }, {
+        path: '/feed', name: 'yourFeed', component: YourFeed
+    }, {
+        path: '/tags/:slug', name: 'tag', component: TagFeed
+    }, {
+        path: '/articles/new', name: 'createArticle', component: CreateArticle
+    }, {
+        path: '/articles/:slug', name: 'article', component: Article
+    }, {
+        path: '/article/:slug/edit', name: 'editArticle', component: GlobalFeed
+    }, {
+        path: '/settings', name: 'settings', component: GlobalFeed
+    }, {
+        path: '/profiles/:slug', name: 'userProfile', component: GlobalFeed
+    }, {
+        path: '/profiles/:slug/favorites', name: 'userProfileFavorites', component: GlobalFeed
+    },]
 
 const router = new VueRouter({
     routes
---
+++ src/views/CreateArticle.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <mcv-article-form
+        :initial-values="initialValues"
+        :errors="validationErrors"
+        :is-submitting="isSubmitting"
+        @articleSubmit="onSubmit"
+    />
+  </div>
+</template>
+
+<script>
+import McvArticleForm from "@/components/ArticleForm"
+
+export default {
+  name: "McvCreateArticle",
+  data() {
+    return {
+      initialValues: {
+        title: '',
+        description: '',
+        body: '',
+        tagList: []
+      },
+      validationErrors: null,
+      isSubmitting: false
+    }
+  },
+  components: {
+    McvArticleForm
+  },
+  methods: {
+    onSubmit: function(form) {
+      console.log(form)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
